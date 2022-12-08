437998cd6fd1e25780729b7f85e05f6c3d471bdc
tort333 <117695969+tort333@users.noreply.github.com>
2022-12-08 16:58:25 -0600

    coommit

--- Develop/script.js
+++ Develop/script.js
@@ -12,3 +12,69 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+var passwordcharactherslower="abcdefghijklmnopqrstuvwxyz";
+var passwordcharacthersupper="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var passwordcharacthersnumbers = "0123456789";
+var passwordcharacthersspecials = "~!@#$%^&*?";
+
+
+function generatePassword() {
+  var actualPassword = "";
+  
+  alert("You will be presented with the following prompts to select criteria for generating a password.");
+
+  
+  var pwdLength = prompt("Choose a number between 8 and 128. This choice will be the length of the password.", );
+  if (pwdLength != null) {
+    if (pwdLength >= 8 && pwdLength <= 128) {
+
+      
+      alert("In the following prompts, at least one choice of lowercase, uppercase, numeric, and/or special characters must be confirmed.");
+
+      var chooser = false;
+      var charChoices = "";
+
+      var ucaracters = confirm("Would you like your password to contain uppercase characters?");
+      if ( === true) {
+        chooser = true;
+        charChoices += passwordcharacthersupper; 
+      }
+
+      var lcaracters = confirm("Would you like your password to contain lowercase characters?");
+      if (lcaracters === true) {
+        chooser = true;
+        charChoices += passwordcharactherslower;
+      }
+
+      var numcaracters= confirm("Would you like your password to contain numeric characters?");
+      if (numcaracters === true) {
+        chooser = true;
+        charChoices += passwordcharacthers;
+      }
+
+      var specialcaracter = confirm("Would you like your password to contain special characters?");
+      if (specialcaracter === true) {
+        chooser = true;
+        charChoices += passwordcharacthers;
+      }
+
+      if (chooser === false) {
+        alert("At least one choice of lowercase, uppercase, numeric, and/or special characters must be confirmed.");
+      } else {
+
+      
+        for (i = 1; i < pwdLength ; i++ ) {
+          actualPassword += charChoices[Math.floor(Math.random()*charChoices.length)];
+        };
+      };
+
+    } else {
+      alert("Please enter a number between 8 and 128")
+    };
+
+  };
+
+  return actualPassword;
+}
